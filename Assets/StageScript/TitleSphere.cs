0cf37cb4c91c148395c72941f587ee782e4b4a42
chelsea <ryuno138@gmail.com>
2020-06-06 18:12:55 +0900

    "タイトル画面アニメーションつける、フォント導入"

---
+++ Assets/StageScript/TitleSphere.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleSphere : MonoBehaviour
+{
+    //public float posY;
+    public Vector3 StartPosition;
+    Rigidbody rigidbody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPosition = transform.position;
+        rigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameObject.transform.position.y < -10)
+        {
+            DropOut();
+        }
+    }
+
+    void DropOut()
+    {
+        transform.position = StartPosition;
+        this.rigidbody.velocity = new Vector3(0, 0, 0);
+    }
+}
